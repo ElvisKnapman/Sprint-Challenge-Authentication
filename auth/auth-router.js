b8bba28133817cba6ab829939bf82adf3ca64c86
Elvis Knapman <elvis@elvisknapman.com>
2019-09-20 12:04:38 -0400

    :sparkles: wrote login handler, implemented jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -31,8 +31,23 @@ router.post("/register", mw.validateBody, async (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", mw.validateBody, async (req, res) => {
   // implement login
+  const { username, password } = req.body;
+
+  try {
+    const user = await Auth.findBy({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error trying to validate user" });
+  }
 });
 
 function generateToken(user) {
