5b8a89ccd75e0454ef6ac501e87489efe20a09ca
Elvis Knapman <elvis@elvisknapman.com>
2019-09-20 19:37:48 -0400

    :white_check_mark: wrote tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,3 +1,4 @@
+// require("dotenv").config();
 const request = require("supertest");
 const server = require("../api/server.js");
 
@@ -5,17 +6,33 @@ describe("POST /register", () => {
   it("should return a 201 status code", async () => {
     const result = await request(server)
       .post("/api/auth/register")
-      .send({ username: "test", password: "testing" });
-
+      .send({ username: "testing1000", password: "testing" });
     expect(result.status).toBe(201);
   });
-
   it("should return an object with new user", async () => {
     const result = await request(server)
       .post("/api/auth/register")
-      .send({ username: "test2", password: "testing2" });
-
+      .send({ username: "test2000", password: "testing2" });
     expect(result.status).toBe(201);
     expect(typeof result.body).toBe("object");
+  });
+});
+
+describe("POST /login", () => {
+  it("should return a 200 status code", async () => {
+    const result = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "testing100", password: "testing" });
+
+    // console.log("RESULT", result);
+
+    expect(result.status).toBe(200);
+  });
+
+  it("should return 400 for invalid body", async () => {
+    const result = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "", password: "" });
+    expect(result.status).toBe(400);
   });
 });
