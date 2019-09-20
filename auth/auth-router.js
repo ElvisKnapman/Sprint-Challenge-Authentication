a4f7dd12a4d9bbb51fd5d9868be3fb4981769188
Elvis Knapman <elvis@elvisknapman.com>
2019-09-20 11:46:35 -0400

    :sparkles: wrote handler for registration

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,49 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
+// middleware
+const mw = require("../middleware/validation.js");
+
+// Auth Model
+const Auth = require("../auth/auth-model.js");
+
+router.post("/register", mw.validateBody, async (req, res) => {
   // implement registration
+  const newUser = req.body;
+
+  try {
+    const hash = bcrypt.hashSync(newUser.password);
+    newUser.password = hash;
+    const user = await Auth.insertUser(newUser);
+
+    console.log("USER", user);
+    if (user) {
+      res.status(201).json(user);
+    } else {
+      res.status(400).json({ message: "Could not complete registration" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Server encountered error while trying to register user"
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
